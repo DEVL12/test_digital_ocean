db909e5144da721636d8ffc9ba124ead2ea98e7c
DEVL12 <Josediaz17822@gmail.com>
2025-06-02 14:42:08 -0400

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         <Image
           className='dark:invert'
@@ -19,7 +19,7 @@ export default function Home() {
           width={130}
         />
         <h1 className='text-2xl'>ESTA VIVO</h1>
-        <ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
+        <ol className='list-inside list-decimal text-sm text-center sm:text-left'>
           <li className='mb-2'>
             Si ves este mensaje Sonny, es que funcionó
             <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
